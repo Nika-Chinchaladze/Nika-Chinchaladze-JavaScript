4702996ac95407fe88ae4bbb0bdb5948989035ac
Nika-Chinchaladze <nika2019chinchaladze@gmail.com>
2024-07-03 13:26:18 +0400

    page load related testing has been completed

--- cypress/e2e/page.cy.js
+++ cypress/e2e/page.cy.js
@@ -10,7 +10,7 @@ describe("Page is loaded", () => {
 
     it("should load - Make up an Algorithm Tab Section successfully", () => {
         cy.visit("/");
-        cy.get("#algorithm-tab").should("exist").should("contain.text", "Make up an Algorithm");
+        cy.get("#algorithm-tab").should("exist").should("contain.text", "Make up an Algorithm").click();
         cy.get("form").should("exist").should("have.length", 3);
         cy.get("#check-number-form").should("exist");
         cy.get("#check-name-form").should("exist");
@@ -25,5 +25,18 @@ describe("Page is loaded", () => {
 
     it("should load - Answer the Questions Tab Section successfully", () => {
         cy.visit("/");
+        cy.get("#question-tab").should("exist").should("contain.text", "Answer the Questions").click();
+        cy.get('[data-cy="given-sequence"]').should("exist").should("contain.text", "Given bracket sequence:");
+        
+        cy.get('[data-cy="first-question"]').should("exist").should("contain.text", "1. Can this sequence be considered correct?");
+        cy.get('[data-cy="first-answer"]').should("exist").as("firstAnswer");
+        cy.get("@firstAnswer").should("contain.text", "Answer: No");
+        cy.get("@firstAnswer").should("contain.text", "Mistake 1:");
+        cy.get("@firstAnswer").should("contain.text", "Mistake 2:");
+
+        cy.get('[data-cy="second-question"]').should("exist").should("contain.text", "2. If the answer to the previous question is");
+        cy.get('[data-cy="second-answer"]').should("exist").as("secondAnswer");
+        cy.get("@secondAnswer").should("contain.text", "Solution 1:");
+        cy.get("@secondAnswer").should("contain.text", "Solution 2:");
     });
 });
