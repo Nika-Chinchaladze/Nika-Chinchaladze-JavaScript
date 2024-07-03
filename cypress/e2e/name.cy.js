9b383f4e8c9faff0b03e1427f2a0201dde7c5336
Nika-Chinchaladze <nika2019chinchaladze@gmail.com>
2024-07-03 12:12:47 +0400

    Name related Algorithm has been tested

--- cypress/e2e/name.cy.js
+++ cypress/e2e/name.cy.js
@@ -7,5 +7,33 @@ describe("Name Algorithm", () => {
         // Act & Execution
         cy.get("#nameInput").click();
         cy.get("#nameInput").type("John");
+        cy.get("#check-name-btn").click();
+        // Assertion
+        cy.get("#name-answer").should("exist");
+        cy.get("#name-answer").should("contain.text", "Hello, John");
+    });
+
+    it("should display - 'There is no such name', if entered name is john (case-sensitive)", () => {
+        // Arrange
+        cy.visit("/");
+        // Act & Execution
+        cy.get("#nameInput").click();
+        cy.get("#nameInput").type("john");
+        cy.get("#check-name-btn").click();
+        // Assertion
+        cy.get("#name-answer").should("exist");
+        cy.get("#name-answer").should("contain.text", "There is no such name");
+    });
+
+    it("should display - 'There is no such name', if entered name does not match John", () => {
+        // Arrange
+        cy.visit("/");
+        // Act & Execution
+        cy.get("#nameInput").click();
+        cy.get("#nameInput").type("Nika");
+        cy.get("#check-name-btn").click();
+        // Assertion
+        cy.get("#name-answer").should("exist");
+        cy.get("#name-answer").should("contain.text", "There is no such name");
     });
 });
